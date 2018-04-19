fd4a582109fcd7aed5f438729cd197e40b40c3d6
trasba <t.rasbach@gmx.net>
2018-04-19 13:35:15 +0200

    modified:   docker-compose.yml 	modified:   rpi-umts/Dockerfile 	modified:   rpi-wifiap/Dockerfile 	modified:   run.sh
    + run.sh was adjusted to set the ip address on wlan0 if it is not set correctly
      further details: first the wlan0 device is flushed and an address x.x.x.1 is set where x.x.x is according to the subnet according to the dhcp-range in dnsmasq.conf
    + rpi-umts/Dockerfile was adjusted because ADD from html doesn't untar
    + rpi-wifiap/Dockerfile was adjusted because hostapd needs an additional setting to use the hostapd.conf file
    + docker-compose.yml just one comment line added (basically for dev purposes)

--- run.sh
+++ run.sh
@@ -5,11 +5,9 @@ function service-cmd {
   command=$2
   case $command in
     start)
-      if [ $(ps aux | grep -v grep | grep $service | wc -l) -gt 0 ]
+      if [ $(ps aux | grep -v grep | grep $service | wc -l) -lt 1 ]
       then
-        echo "OK $service is running"
-      else
-        echo "starting $service"
+        echo "$service not running -> starting..."
         service $service start
       fi
       ;;
@@ -17,10 +15,8 @@ function service-cmd {
     stop)
       if [ $(ps aux | grep -v grep | grep $service | wc -l) -gt 0 ]
       then
-        echo "stopping $service"
+        echo "$service is running -> stopping..."
         service $service stop
-      else
-        echo "OK $service is not running"
       fi
       ;;
     restart)
@@ -37,16 +33,36 @@ function interface-up {
     echo "Interface $interface down -> bringing it up..."
     service-cmd "hostapd" restart
     ip link set dev $interface up
-  else
-    echo "Interface $interface is up"
   fi
 }
 
+function set-ip {
+  ipaddr=$(cat /etc/dnsmasq.conf|grep dhcp-range|grep -Eo '([0-9]{1,3}\.){3}'|head -1)1
+  case $1 in
+    'f')
+      # f = force, flush and write address
+      echo "Force flushing wlan0 and setting addr..."
+      ip addr flush dev wlan0
+      ip addr add $ipaddr/24 dev wlan0 
+      ;;
+    'c')
+      # c = check, first check then possibly flush and write address
+      if [ $(ip addr|grep -E $ipaddr|wc -l) -lt 1 ]
+      then
+        echo "Ip address not set correctly -> flushing wlan0 and setting address..."
+        ip addr flush dev wlan0
+        ip addr add $ipaddr/24 dev wlan0 
+      fi
+      ;;
+  esac
+}
+
+set-ip f
 while [ 1 ]
 do
   interface-up "wlan0"
-  #dnsmasq
   service-cmd "dnsmasq" start
   service-cmd "hostapd" start
-  sleep 3s
+  set-ip c
+  sleep 15s
 done
